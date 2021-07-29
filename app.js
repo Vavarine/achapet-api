d0b414ebb14a45e5de20d10ed33fd35249efd264
vavarine <evailson.m.barbosa@gmail.com>
2021-07-29 19:36:59 -0300

    eva-commit

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('../achapet-server/mongoDB/bd');
+const connectDB = require("../achapet-server/mongoDB/bd");
 
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
+
+app.use("/", (req, res) => {
+  return res.json({
+    message: "boa meu garoto !",
+  });
+});
 
 // var usersRouter = require('./routes/users');
 
-require('../achapet-server/routes/users')(app);
+require("../achapet-server/routes/users")(app);
 require("dotenv").config();
 
 // app.use(express.json());
@@ -22,9 +29,8 @@ require("dotenv").config();
 // app.use('/users', usersRouter);
 
 connectDB();
-server = app.listen(process.env.PORT || 3003, () => {
-  console.log('Servidor online!')   
-})
+server = app.listen(process.env.PORT || 3030, () => {
+  console.log("Servidor online!");
+});
 
 module.exports = app;
-
